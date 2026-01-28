4b59b545079f29fcbf845d96f4fc63c388874167
sujal sachdeva <sujalsachdeva0001@gmail.com>
2026-01-28 23:20:20 +0530

    [Sujal] Print OOPS using String.join

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,24 +1,37 @@
 /**
- * UC2: Print OOPS banner using print statements
- * Each letter is 7 lines tall and properly aligned
+ * UC3: Render OOPS banner using String.join()
+ * Uses String.join instead of + for better memory efficiency
  */
 public class OopsBannerApp {
 
     public static void main(String[] args) {
 
         // Line 1
-        System.out.println("  *****     *****     *****      ***** ");
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", "  *****  ", "  *****  "));
+
         // Line 2
-        System.out.println(" *     *   *     *   *     *    *       ");
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *     * ", " *       "));
+
         // Line 3
-        System.out.println(" *     *   *     *   *     *    *       ");
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *     * ", " *       "));
+
         // Line 4
-        System.out.println(" *     *   *     *   *****      *****  ");
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *****   ", "  *****  "));
+
         // Line 5
-        System.out.println(" *     *   *     *   *               * ");
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *       ", "       * "));
+
         // Line 6
-        System.out.println(" *     *   *     *   *               * ");
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *       ", "       * "));
+
         // Line 7
-        System.out.println("  *****     *****    *          *****  ");
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", " *       ", "  *****  "));
     }
 }
